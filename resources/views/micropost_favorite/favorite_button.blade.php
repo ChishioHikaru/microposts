dcdb827d028caecda9bf344631f3c2c5217973de
sonjun.kimu <hopemukku7@gmail.com>
2021-06-24 13:33:52 +0000

    microposts-button

--- resources/views/micropost_favorite/favorite_button.blade.php
+++ resources/views/micropost_favorite/favorite_button.blade.php
@@ -1,13 +1,11 @@
-@if (Auth::id() != $micropost->id)
-    @if (Auth::user()->is_favorite($micropost->id))
-        {{-- アンフェイバリットーボタンのフォーム --}}
-        {!! Form::open(['route' => ['micropost.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('UnFavorite', ['class' => "btn btn-success btn-sm"]) !!}
-        {!! Form::close() !!}
-    @else
-        {{-- フェイバリットボタンのフォーム --}}
-        {!! Form::open(['route' => ['micropost.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
-        {!! Form::close() !!}
-    @endif
+@if (Auth::user()->is_favorite($micropost->id))
+    {{-- アンフェイバリットーボタンのフォーム --}}
+    {!! Form::open(['route' => ['micropost.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+        {!! Form::submit('UnFavorite', ['class' => "btn btn-success btn-sm"]) !!}
+    {!! Form::close() !!}
+@else
+    {{-- フェイバリットボタンのフォーム --}}
+    {!! Form::open(['route' => ['micropost.favorite', $micropost->id]]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
+    {!! Form::close() !!}
 @endif
